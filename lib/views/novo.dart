be3a957359b043299d1b4c5f028104a60bf03547
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-10-01 08:18:53 -0300

    Add TextField

--- lib/views/novo.dart
+++ lib/views/novo.dart
@@ -11,9 +11,32 @@ class NovoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Novo Contato")),
-      body: ElevatedButton(
-        onPressed: () => salvar(context),
-        child: Text("Salvar")
+      body: Container(
+        margin: EdgeInsets.all(12),
+        child: Column(
+          spacing: 10,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                filled: false,
+                labelText: "Nome",
+                border: OutlineInputBorder()
+              ),
+            ),
+            TextField(
+              obscureText: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                border: OutlineInputBorder()
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => salvar(context),
+              child: Text("Salvar")
+            ),
+          ],
+        ),
       )
     );
   }
